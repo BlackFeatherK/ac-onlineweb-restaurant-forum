fbbb8889b68307fda3e070ba47ab44c58237e6b9
jimmy <jimmyyang5296@gmail.com>
2017-11-13 10:20:39 +0800

    修改阿暐 code review 回來的 code 建議。

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class Admin::RestaurantsController < ApplicationController
 	before_action :authenticate_user!  # 這個是 devise 提供的方法，先檢查必須登入
 	before_action :authenticate_admin # 再檢查是否有權限
-	before_action :set_restaurant,  :only => [ :show, :edit, :update, :destroy]
+	before_action :set_restaurant, :only => [:show, :edit, :update, :destroy]
 
 	def index
     	@restaurants = Restaurant.all
@@ -14,10 +14,11 @@ class Admin::RestaurantsController < ApplicationController
 	def create
   	@restaurant = Restaurant.new(restaurant_params)
     if @restaurant.save
-      redirect_to :action => :index
+      redirect_to admin_restaurants_path
 			flash[:notice] = "restaurant was successfully created"
     else
- 			render :action => :new
+			render :new
+			flash[:notice] = "restaurant was failed to created"
     end
   end
 
@@ -26,29 +27,32 @@ class Admin::RestaurantsController < ApplicationController
 
   def update
    if @restaurant.update(restaurant_params)
-      redirect_to :action => :show, :id => @restaurant
+      redirect_to admin_restaurant_path(@restaurant)
       flash[:notice] = "restaurant was successfully updated"
    else
-      render :action => :edit
+      render :edit
+			flash[:notice] = "restaurant was failed to update"
    end
   end
 
   def show
   end
 
+	def destroy
+		@restaurant.destroy
+		redirect_to admin_restaurants_path
+	end
 
 
   private
 
+	def set_restaurant
+		@restaurant = Restaurant.find(params[:id])
+	end
+
   def restaurant_params
-    params.require(:restaurant).permit(:name,
-    																	:opening_hours,
-    																	:tel,
-    																	:address,
-    																	:description,
-    																	:image,
-                                      :category_id
-    																	)
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address,
+    																	:description, :image, :category_id)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
     collection do
       get :ranking
     end
-    
+
   end
 
   resources :categories do
